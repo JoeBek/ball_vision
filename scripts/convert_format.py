d38eba0f14348b7fa1d40a40fd1db8496207c526
daniel <dtr@vt.edu>
2025-07-01 15:07:17 -0400

    bb

--- scripts/convert_format.py
+++ scripts/convert_format.py
@@ -1,6 +1,6 @@
 from pylabel import importer
 
 
-dataset = importer.ImportVOC("golf_ball/Detection/Annotations",
+dataset = importer.ImportVOC("golf_ball/training/Detection/Annotations",
                              path_to_images="../JPEGImages")
 dataset.export.ExportToYoloV5(use_splits=True, output_path="training_1/labels", copy_images=True)
