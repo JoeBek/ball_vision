133137338e73def23ca2f2004ba5c45f7428757f
joe <jb5@vt.edu>
2025-07-02 12:40:52 -0400

    adding clutter

--- scripts/convert_format.py
+++ scripts/convert_format.py
@@ -1,6 +1,6 @@
 from pylabel import importer
 
 
-dataset = importer.ImportVOC("golf_ball/training/Detection/Annotations",
+dataset = importer.ImportVOC("training/Detection/Annotations",
                              path_to_images="../JPEGImages")
 dataset.export.ExportToYoloV5(use_splits=True, output_path="training_1/labels", copy_images=True)
